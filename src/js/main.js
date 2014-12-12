c99f7a73621f0ca080369e3b4499d3ff8b0d5a2f
Dmitri Gaisan <dima.gaisan@gmail.com>
2014-12-12 02:43:11 -0600

    Adding remove functionality

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,27 @@ var React = require('react');
 
 var TodoItems = React.createClass({
 
+    /**
+     * Invoking a callback(passed via props) in order to remove item from a 'model'
+     * @param index item to be removed
+     * @returns {function(this:TodoItems)}
+     */
+    handleDelete: function(index) {
+        return function() {
+            this.props.onDelete(index);
+        }.bind(this);
+    },
+
     render: function() {
+        var onDelete = this.handleDelete;
         return (
             <ul>
                 {this.props.items.map(function(item, i) {
                     return (
-                        <li key={i}>{item}</li>
+                        <li key={i}>
+                            {item}
+                            <button className="btn btn-danger" onClick={onDelete(i)}>x</button>
+                        </li>
                     );
                 })}
             </ul>
@@ -30,7 +45,7 @@ var TodoApp = React.createClass({
             this.refs['newTask'].getDOMNode().value;
         if (newTask) {
             var updatedItems = this.state.items.concat([newTask]);
-            this.setState(updatedItems);
+            this.setState({items: updatedItems});
             this.cleanUpTextField();
         }
     },
@@ -39,10 +54,20 @@ var TodoApp = React.createClass({
         this.refs['newTask'].getDOMNode().value = '';
     },
 
+    /**
+     * Removes an Item from a list of Items
+     * @param index index or an item to be removed
+     */
+    handleDelete: function(index) {
+        var updatedItems = this.state.items.splice(0);
+        updatedItems.splice(index, 1);
+        this.setState({items: updatedItems});
+    },
+
     render: function() {
         return (
             <div>
-                <TodoItems items={this.state.items} />
+                <TodoItems items={this.state.items} onDelete={this.handleDelete} />
                 <input type="text" ref='newTask' />
                 <button onClick={this.handleClick}>Add Task</button>
             </div>
